667314075529b8a0348e307ca788562a94f53433
mifouts <fouts.mitch15@gmail.com>
2022-08-22 11:59:32 -0400

    adding overlay visible

--- index.js
+++ index.js
@@ -4,17 +4,18 @@
 
 function contact(event) {
     event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_8bgbkyi',
-            'template_lya0z0l',
-            event.target,
-            'E709MmtjUQvYhM8xQ'
-        ).then(() => {
-            console.log('this worked')
-        })
+   // emailjs
+       // .sendForm(
+          //  'service_8bgbkyi',
+          //  'template_lya0z0l',
+          //  event.target,
+          //  'E709MmtjUQvYhM8xQ'
+        //).then(() => {
+           // console.log('this worked')
+       // })
         const loading = document.querySelector('modal__overlay--loading');
         const success = document.querySelector('modal__overlay--success');
+        loading.classList += " modal__overlay--visible"
         setTimeout(() => {
             console.log('this worked')
         }, 200);
