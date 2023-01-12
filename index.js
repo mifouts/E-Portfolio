25a616687817685064d22f41f073ee9114b26350
mifouts <fouts.mitch15@gmail.com>
2023-01-12 15:42:10 -0500

    updating portfolio

--- index.js
+++ index.js
@@ -4,62 +4,56 @@
 
 let contrastToggle = false;
 let isModalOpen = false;
-const scaleFactor = 1/20;
-
-
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape");
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-    
-    for (let i = 0; i < shapes.length; ++i) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px`;
-}
-}
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px`;
+  }
+}
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle){
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
     document.body.classList += " dark-theme";
-    }
-    else {
-        document.body.classList.remove("dark-theme");
-    }
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
-
 function contact(event) {
-    event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_8bgbkyi',
-            'template_lya0z0l',
-            event.target,
-            'E709MmtjUQvYhM8xQ'
-        ).then(() => {
-            console.log('this worked')
-        })
-        const loading = document.querySelector('.modal__overlay--loading');
-        const success = document.querySelector('.modal__overlay--success');
-        loading.classList += " modal__overlay--visible";
-        setTimeout(() => {
-            loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";
-            console.log('this worked')
-        }, 200);
+  event.preventDefault();
+  emailjs
+    .sendForm(
+      "service_8bgbkyi",
+      "template_lya0z0l",
+      event.target,
+      "E709MmtjUQvYhM8xQ"
+    )
+    .then(() => {
+      console.log("this worked");
+    });
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  setTimeout(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    console.log("this worked");
+  }, 200);
+}
 
-} 
-
-
-function toggleModal(){
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
